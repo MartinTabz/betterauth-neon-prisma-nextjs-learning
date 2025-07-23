1fba7844f38a7b69e551ae1bac3d19fd377e2eb2
Martin Šíl <mail@martinsil.cz>
2025-07-23 12:16:27 +0200

    chore: add global Providers wrapper to app layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Providers from "@/components/Providers";
 
 export const metadata: Metadata = {
 	title: "Better Auth | Neon Tech | Prisma ORM | NextJS Learning Application",
@@ -13,7 +14,9 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="cs">
-			<body className={`antialiased`}>{children}</body>
+			<body className={`antialiased`}>
+				<Providers>{children}</Providers>
+			</body>
 		</html>
 	);
 }
